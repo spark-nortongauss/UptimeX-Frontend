b7c2a3e747f0b2c24ab48c43f0530c6d3a543b50
FARDIN98 <fardinahmed66@gmail.com>
2025-10-04 17:40:53 +0600

    feat: add success message to home page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex justify-center items-center h-screen">
       <div className="text-center">
         <h1>Finally deployment successful. Yaaay!!!!</h1>
+        <h2>Its Working</h2>
       </div>
     </div>
   );
