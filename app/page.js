ddc49110407fb06db7141c3a972a443fa6bc5153
FARDIN98 <fardinahmed66@gmail.com>
2025-10-04 18:17:45 +0600

    style(page): update text styling and structure in home page
    Refactor text elements to use semantic HTML tags and adjust font weights for better visual hierarchy

--- app/page.js
+++ app/page.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="text-center">
-        <h1>Finally deployment successful. Yaaay!!!!</h1>
-        <h2>Its Working</h2>
+      <div className="font-semibold">
+        <h6>Finally deployment successful. Yaaay!!!!</h6>
+      </div>
+      <div className="font-bold">
+        <p>Testing</p>
       </div>
     </div>
   );
